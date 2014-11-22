8698ab3bdb4a94d311f39c36587255195d266cb7
joost sijm <joostsijm@gmail.com>
2014-11-22 11:34:53 +0100

    menu item

--- pages/menu.php
+++ pages/menu.php
@@ -1,3 +1,2 @@
 <li class=menuitem><a class=mlink href=../pages/index.php>Home</a></li>
 <li class=menuitem><a class=mlink href=../pages/niews.php>Niews</a></li>
-<li class=menuitem><a class=mlink href=></a></li>
