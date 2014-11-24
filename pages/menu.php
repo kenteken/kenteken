d7065b62532cc35a7d92c13f9e28640304bd9384
Joost Sijm <joostsijm@gmail.com>
2014-11-24 15:28:32 +0100

    spellings fout fix

--- pages/menu.php
+++ pages/menu.php
@@ -1,2 +1,2 @@
 <li class=menuitem1><a class=mlink href=../pages/index.php>Home</a></li>
-<li class=menuitem2><a class=mlink href=../pages/niews.php>Niews</a></li>
+<li class=menuitem2><a class=mlink href=../pages/nieuws.php>Nieuws</a></li>
