252fae557d8a8ae87f3d9880842158e2a5553d8d
Joost Sijm <joostsijm@gmail.com>
2014-11-26 12:23:28 +0100

    voor stefan

--- pages/index.php
+++ pages/index.php
@@ -37,7 +37,7 @@
 				<li>Uitleg</li>
 				<li><a href="../media/Daniboy.png">Dani</a><br></li>
 				<li>En nog meer!</li>
-				<!--<li><a href="https://7chan.org/fl/src/SuperDeepthroat1_9_1b.swf">vlinders</a></li>-->
+				<li><a href="https://7chan.org/fl/src/SuperDeepthroat1_9_1b.swf">vlinders</a></li>
 			</ul>
 			<h2 id=link>Handige links</h2>
 			<p>
